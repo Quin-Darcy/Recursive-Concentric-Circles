551d53c82121ca341bb449fa09db02fe219baf6b
Quin Darcy <75754352+Quin-Darcy@users.noreply.github.com>
2021-07-16 03:34:54 +0000

    Collision and move work. Need to work on internal collision (parent wall with child)

--- ball.js
+++ ball.js
@@ -1,25 +1,27 @@
 class Ball {
-    constructor(x, y, id, layer, outer_balls, balls) {
+    constructor(x, y, id, layer, parents, siblings, pid) {
         this.x = x; 
         this.y = y;
         this.id = id;
         this.layer = layer;
-        this.outer_balls = outer_balls; 
-        this.balls = balls; 
-        this.inner_balls = [];
-        this.v_x = 0; 
-        this.v_y = 0;
+        this.parents = parents; 
+        this.pid = pid;
+        this.siblings = siblings; 
+        this.children = [];
+        this.child_num = random(1, 5);
+        this.v_x = random(-1, 1); 
+        this.v_y = random(-1, 1);
         this.area = 0;
         this.r = 0;
         if (this.layer === 0) {
             this.r = R;
             this.area = Math.PI * Math.pow(this.r, 2);
         } else {
-            this.r = Math.sqrt(A_RATIO * this.outer_balls[0].area / (Math.PI * BALL_NUM));
+            this.r = Math.sqrt(A_RATIO * this.parents[0].area / (Math.PI * this.parents[this.pid].child_num));
             this.area = Math.PI * Math.pow(this.r, 2);
         }
     }
-    set_inner_balls() {
+    set_children() {
         if (this.layer + 1 < LAYERS) {
             let x = 0;
             let y = 0;
@@ -27,25 +29,89 @@ class Ball {
             let H_x = this.x + this.r / 4;
             let L_y = this.y - this.r / 4;
             let H_y = this.y + this.r / 4;
-            for (let i = 0; i < BALL_NUM; i++) {
+            for (let i = 0; i < this.child_num; i++) {
                 x = random(L_x, H_x);
                 y = random(L_y, H_y);                              
-
-                this.inner_balls[i] = new Ball(x, y, i, this.layer+1, this.balls, this.inner_balls);
-                this.inner_balls[i].set_inner_balls();
+                this.children[i] = new Ball(x, y, i, this.layer+1, this.siblings, this.children, this.id);
+                this.children[i].set_children();
             }
         } else {
             return;
+        }
+    }
+    collision() {
+        for (let i = this.id + 1; i < this.siblings.length; i++) {
+            let dx = this.siblings[i].x - this.x;
+            let dy = this.siblings[i].y - this.y;
+            let center_dist = dist(dx, dy, 0, 0);
+            let touching_dist = this.siblings[i].r + this.r;
+            if (center_dist < touching_dist) {
+                let theta = atan2(dy, dx);
+                let new_x = this.x + cos(theta) * touching_dist;
+                let new_y = this.y + sin(theta) * touching_dist;
+                let a_x = (new_x - this.siblings[i].x) * cor;
+                let a_y = (new_y - this.siblings[i].y) * cor;
+                this.v_x -= a_x*hardness;
+                this.v_y -= a_y*hardness;
+                this.siblings[i].v_x += a_x*hardness;
+                this.siblings[i].v_y += a_y*hardness; 
+            }
+        }
+    }
+
+    move() {
+        if (this.layer === 0) {
+            this.v_y += g;
+            this.x += this.v_x;
+            this.y += this.v_y;
+            if (this.x + this.r > W) {
+                this.x = W - this.r;
+                this.v_x = this.v_x*cor*mu;
+            } else if (this.x - this.r < 0) {
+                this.x = this.r;
+                this.v_x = this.v_x*cor*mu;
+            }
+            if (this.y + this.r >= H) {
+                this.y = H - this.r;
+                this.v_y = this.v_y*cor*mu;
+            } else if (this.y - this.r < 0) {
+                this.y = this.r;
+                this.v_y = this.v_y*cor*mu;
+            }
+        } else {
+            this.v_y += g;
+            this.x += this.v_x;
+            this.y += this.v_y;
+            let L = this.parents[0].r;
+            let C = createVector(this.parents[this.pid].x, this.parents[this.pid].y);
+            let P = createVector(this.x, this.y);
+            let D = C.dist(P);
+            if (D >= L - this.r) {
+                let theta1 = atan2(this.parents[this.pid].y - this.y, this.parents[this.pid].x - this.x);
+                let theta2 = Math.PI - atan2(-this.v_y, this.v_x);
+                this.x = this.parents[this.pid].x - (L - this.r) * cos(theta1);
+                this.y = this.parents[this.pid].y - (L - this.r) * sin(theta1);
+                this.v_x = this.v_x*cor*mu;
+                this.v_y = this.v_y*cor*mu;
+                let V = createVector(this.v_x, this.v_y);
+                V.setHeading(theta2);
+                this.v_x = V.x;
+                this.v_y = V.y;
+                this.parents[this.pid].v_x = -this.v_x;
+                this.parents[this.pid].v_y = -this.v_y;
+            }
         }
     }
     show() { 
         if (this.layer < LAYERS) {
             stroke(255);
             noFill();
-            ellipse(this.x, this.y, this.r);
+            ellipse(this.x, this.y, 2*this.r);
             if (this.layer + 1 < LAYERS) {
-                for (let i = 0; i < BALL_NUM; i++) {
-                    this.inner_balls[i].show();
+                for (let i = 0; i < this.children.length; i++) {
+                    this.children[i].collision();
+                    this.children[i].move();
+                    this.children[i].show();
                 }
             }
         } else {
--- script.js
+++ script.js
@@ -3,10 +3,16 @@ let W = window.innerWidth;
 let H = window.innerHeight;
 
 // Object Constants
-let LAYERS = 3;
-let R = 200;
-let A_RATIO = 0.1;
+let LAYERS = 5;
+let R = 150;
+let A_RATIO = 0.25;
 let BALL_NUM = 3;
+
+// Environment Constants
+let g = 0/10;
+let mu = -0.9;
+let hardness = 1;
+let cor = 0.99;
 
 // Arrays 
 balls = [];
@@ -15,15 +21,17 @@ function setup() {
     createCanvas(W, H);
     background(0);
     for (let i = 0; i < BALL_NUM; i++) {
-        balls[i] = new Ball(random(R, W - R), random(R, H - R), i, 0, [], balls); 
-        balls[i].set_inner_balls();
-        
+        balls[i] = new Ball(random(R, W - R), random(R, H - R), i, 0, [], balls, 0); 
+        balls[i].set_children();   
     }
 }
 
 function draw() {
+    frameRate(10);
     background(0);
     for (let i = 0; i < BALL_NUM; i++) {
+        balls[i].collision();
+        balls[i].move();
         balls[i].show();
     }
 }
